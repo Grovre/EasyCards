dc0e08b6dcb8e65a35acaca0a85abea14433098a
Grovre <50428844+Grovre@users.noreply.github.com>
2022-02-26 21:12:40 -0600

    Converted from array to ArrayList

--- src/grovre/java/Deck.java
+++ src/grovre/java/Deck.java
@@ -5,11 +5,14 @@ import grovre.java.suits.Diamond;
 import grovre.java.suits.Heart;
 import grovre.java.suits.Spade;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
 public class Deck {
 
-    private final Card[] deck;
+    private final ArrayList<Card> deck;
     private int topOfDeckIndex = 0;
 
     public Deck() {
@@ -33,31 +36,32 @@ public class Deck {
         return str.toString();
     }
 
-    public Card[] generateNewDeck(boolean faceCardsAllEqual10) {
+    // TODO: 2/26/22 Improve deck generation now that this no longer uses arrays but a list instead
+    public ArrayList<Card> generateNewDeck(boolean faceCardsAllEqual10) {
         Card[] tempDeck = new Card[52];
         for (int i = 0; i < 13; i++) tempDeck[i] = new Spade(i, faceCardsAllEqual10);
         for (int i = 13; i < 13 * 2; i++) tempDeck[i] = new Club(i - 13, faceCardsAllEqual10);
         for (int i = 13 * 2; i < 13 * 3; i++) tempDeck[i] = new Heart(i - 13 * 2, faceCardsAllEqual10);
         for (int i = 13 * 3; i < 13 * 4; i++) tempDeck[i] = new Diamond(i - 13 * 3, faceCardsAllEqual10);
-        return tempDeck;
+        return (ArrayList<Card>) Arrays.asList(tempDeck);
     }
 
     @Override
     public String toString() {
         return "Deck{" +
-                "deck=" + Arrays.toString(deck) +
+                "deck=" + deck.toString() +
                 '}';
     }
 
     public String toStringClean() {
-        StringBuilder str = new StringBuilder(deck[0].toStringClean());
-        for (int i = 1; i < deck.length; i++) {
-            str.append(", ").append(deck[i].toStringClean());
+        StringBuilder str = new StringBuilder(deck.get(0).toStringClean());
+        for (int i = 1; i < deck.size(); i++) {
+            str.append(", ").append(deck.get(i).toStringClean());
         }
         return str.toString();
     }
 
-    public Card[] getDeck() {
+    public ArrayList<Card> getDeck() {
         return deck;
     }
 
@@ -79,12 +83,13 @@ public class Deck {
         }
     }
 
+    @Deprecated
     public void randomizeDeck() {
-        for (int i = 0; i < deck.length; i++) {
-            Card tempCard = deck[i];
-            int tempIndex = (int) (Math.random() * deck.length);
-            deck[i] = deck[tempIndex];
-            deck[tempIndex] = tempCard;
+        for (int i = 0; i < deck.size(); i++) {
+            Card tempCard = deck.get(i);
+            int tempIndex = (int) (Math.random() * deck.size());
+            deck.set(i, deck.get(tempIndex));
+            deck.set(tempIndex, tempCard);
         }
         topOfDeckIndex = 0;
     }
@@ -109,7 +114,7 @@ public class Deck {
 
     public Card takeTopOfDeck() {
         topOfDeckIndex++;
-        return deck[topOfDeckIndex - 1];
+        return deck.get(topOfDeckIndex - 1);
     }
 
     public Card getTopOfDeck() {
@@ -117,27 +122,27 @@ public class Deck {
     }
 
     public void moveTopCardTo(int deckIndex) {
-        Card temp = deck[topOfDeckIndex];
-        if (deckIndex - topOfDeckIndex >= 0)
-            System.arraycopy(deck, topOfDeckIndex + 1, deck, topOfDeckIndex, deckIndex - topOfDeckIndex);
-        deck[deckIndex] = temp;
+        Card temp = deck.remove(topOfDeckIndex);
+        topOfDeckIndex--;
+        deck.add(deckIndex, temp);
     }
 
     public void swapTopCardWith(int indexOfCardToSwap) {
-        Card temp = deck[topOfDeckIndex];
-        deck[topOfDeckIndex] = deck[indexOfCardToSwap];
-        deck[indexOfCardToSwap] = temp;
+        Card temp = deck.get(topOfDeckIndex);
+        deck.set(topOfDeckIndex, deck.get(indexOfCardToSwap));
+        deck.set(indexOfCardToSwap, temp);
     }
 
     public void swapCards(int firstIndex, int secondIndex) {
-        Card temp = deck[firstIndex];
-        deck[firstIndex] = deck[secondIndex];
-        deck[secondIndex] = temp;
+        Card temp = deck.get(firstIndex);
+        deck.set(firstIndex, deck.get(secondIndex));
+        deck.set(secondIndex, temp);
     }
 
+    // TODO: 2/26/22 Revisit and make this method better
     public int getIndexOfCard(int valueOrder, int suitOrder) {
-        for (int i = 0; i < deck.length; i++) {
-            Card c = deck[i];
+        for (int i = 0; i < deck.size(); i++) {
+            Card c = deck.get(i);
             if (c.getVALUE_ORDER() == valueOrder && c.getSUIT_ORDER() == suitOrder) return i;
         }
         System.out.println("Card type doesn't exist.");
@@ -147,14 +152,14 @@ public class Deck {
     }
 
     public Card getCardAtIndex(int indexOfCard) {
-        return deck[indexOfCard];
+        return deck.get(indexOfCard);
     }
 
-    public Card[] getDeckRange(int firstIndex, int lastIndex) {
-        return Arrays.copyOfRange(deck, firstIndex, lastIndex);
+    public ArrayList<Card> getDeckRange(int firstIndex, int lastIndex) {
+        return new ArrayList<>(deck.subList(firstIndex, lastIndex));
     }
 
     public Card getRandomCardFromDeck() {
-        return deck[(int) (Math.random() * deck.length)];
+        return deck.get(new Random().nextInt(deck.size()));
     }
 }
